68d76741a5600df2c456ae6cf2f74c028e96e2f3
szniobo <122813024+FastFoot3@users.noreply.github.com>
2025-01-06 22:40:15 +0100

    komentarze i uporządkowanie
    jak w tytule

--- eye_detect.py
+++ eye_detect.py
@@ -42,20 +42,30 @@ eye_bin_mopen_mclose = [None, None] # Lista przechowująca obraz binarny po oper
 
 start_time = time.time() # Pobranie czasu rozpoczęcia programu
 
+
 # Otwarcie pliku do zapisu pozycji źrenic
 with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzanie zasobami jak się kończy to automatycznie zamyka plik
-    file.write('Eye_Index time X_Displacement Y_Displacement\n')  # Nagłówek pliku
+    
+    
+    # Nagłówek pliku
+    file.write('Eye_Index time X_Displacement Y_Displacement\n')
 
+    
     while True: # Główna pętla programu
+        
+        
         ret, frame = cap.read() # Pobranie klatki obrazu do frame, ret to wartość bool która informuje czy udało się pobrać klatkę
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Konwertuje klatkę z kamery na grayscale
 
         # Wykrywanie twarzy
         faces = face_cascade.detectMultiScale(gray, 1.3, 5) # skalowanie obrazu 1.3, liczba trafień żeby uznać twarz 5 # faces to prostokąty
 
+        
+        # Dla każdej twarzy
         for (x, y, w, h) in faces: # współrzędna X górnego lewego rogu prostokąta # współrzędna Y górnego lewego rogu prostokąta # szerokość prostokąta # wysokość prostokąta # całość dla każdej twarzy
 
             cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 255), 2) # rysowanie twarzy
+
 
             """W Pythonie, korzystając z biblioteki OpenCV, obraz jest reprezentowany jako macierz pikseli (tablica 2D lub 3D)
             Pierwszy indeks (przed przecinkiem) oznacza współrzędną pionową (oś Y) czyli linie (rzędy) pikseli.
@@ -71,19 +81,26 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
 
             # Wykrywanie oczu
             eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.05, minNeighbors=10) # skalowanie obrazu 1.05, liczba trafień żeby uznać twarz 10 # eyes to prostokąty
+            
+            
+            # Dla każdego oka
             for i, (ex, ey, ew, eh) in enumerate(eyes[:2]):  # enumerate indeksuje sekwencję (i), a [:2] zwraca wycinek listy do 2 (bez niego, czyli 0 i 1) # współrzędne jak w twarzach
+                
                 eye_gray = roi_gray[ey:ey + eh, ex:ex + ew]
                 eye_color = roi_color[ey:ey + eh, ex:ex + ew]
 
+                # Rysowanie prostokąta wokół oczu (jest to obszar eye_gray)
+                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 255), 2)
 
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 255), 2) # rysowanie oczu
 
-                # Obliczenie środka oka
+                # Obliczenie środka oka względem wycinka eye_gray
                 eye_center_x = ew // 2 # obliczenie współrzędnej x
                 eye_center_y = eh // 2 # obliczenie współrzędnej y
 
-                cv2.line(eye_color, (0, eye_center_y), (ew, eye_center_y), (0, 0, 255), 1) # rysowanie środka
-                cv2.line(eye_color, (eye_center_x, 0), (eye_center_x, eh), (0, 0, 255), 1) # rysowanie środka
+                # Rysowanie środka oka
+                cv2.line(eye_color, (0, eye_center_y), (ew, eye_center_y), (0, 0, 255), 1)
+                cv2.line(eye_color, (eye_center_x, 0), (eye_center_x, eh), (0, 0, 255), 1)
+
 
                 """działanie retval, dst = cv2.threshold(src, thresh, maxval, type)
                 retval (symbol _) - Zwraca wartość użytego progu (można ją pominąć, stąd _)
@@ -93,6 +110,7 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
                 maxval (255) - Maksymalna wartość, którą piksele przyjmują (tutaj biały)
                 type (cv2.THRESH_BINARY_INV) - Rodzaj progu (tutaj pracujemy na negatywie, czyli piksele ciemniejsze od 30 dają 1)"""
 
+                # Progowanie obrazu (binaryzacja)
                 _, eye_bin[i] = cv2.threshold(eye_gray, thresh, 255, cv2.THRESH_BINARY_INV)
 
                 # Tworzymy kernel (macierz do operacji morfologicznych)
@@ -104,13 +122,21 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
                 # Wypełnianie małych dziur (zamknięcie) - Wypełnia dziury w obiektach, wygładza krawędzie obiektów (nie usuwa szumów)
                 eye_bin_mopen_mclose[i] = cv2.morphologyEx(eye_bin_mopen, cv2.MORPH_CLOSE, kernel)
 
+
                 """cv2.findContours - Znajdowanie konturów (Tworzy listę współrzędnych punktów (x, y) dla każdego konturu)
                     cv2.RETR_TREE - Zbiera wszystkie kontury, w tym również te zagnieżdżone
                     cv2.CHAIN_APPROX_SIMPLE - Upraszcza kontury, przechowując tylko kluczowe punkty"""
 
+                # Znajdowanie konturów na obrazie binarnym
                 contours, _ = cv2.findContours(eye_bin_mopen_mclose[i], cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+                # Sprawdzenie, czy znaleziono jakieś kontury
                 if contours:
+
+                    # Znaleziono kontury, wybieramy największy kontur
                     largest_contour = max(contours, key=cv2.contourArea) # Największy kontur prawdopodobnie odpowiada źrenicy
+
+                    # Obliczenie środka i promienia okręgu otaczającego kontur
                     (cx, cy), radius = cv2.minEnclosingCircle(largest_contour) # Znajduje najmniejszy okrąg otaczający kontur i zwraca jego środek i promień (źrenicę)
                     
                     """# Obliczanie momentów konturu
@@ -123,8 +149,13 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
                     else:
                         cx, cy = None, None"""
 
+                    # Rysowanie okręgu wokół źrenicy
                     cv2.circle(eye_color, (int(cx), int(cy)), int(radius), (0, 0, 255), 2) # Rysowanie czerwonego okręgu wokół wykrytej źrenicy
                     # !!!W OPENCV KOLORY SĄ ZAPISYWANE JAKO BGR A NIE RGB (kto to wymyślił w ogóle smh)!!!
+
+                    # Rysowanie źrenicy
+                    cv2.circle(eye_color, (int(cx), int(cy)), 5, (255, 0, 0), -1) # Mały niebieski punkt powinien śledzić środek źrenicy
+
 
                     # Obliczenie przemieszczenia względem środka oka
                     displacement_x = cx - eye_center_x
@@ -133,12 +164,8 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
                     # Obliczenie czasu działania programu
                     program_runtime = time.time() - start_time
 
-
                     # Zapis danych do pliku
                     file.write(f'{i} {program_runtime:.2f} {displacement_x:.2f} {displacement_y:.2f}\n')
-
-                    # Rysowanie źrenicy
-                    cv2.circle(eye_color, (int(cx), int(cy)), 5, (255, 0, 0), -1) # Mały niebieski punkt powinien śledzić środek źrenicy
 
 
         # Wyświetlanie oczu binarnych
@@ -149,8 +176,9 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
             eye_bin_mopen_mclose[0] = cv2.resize(eye_bin_mopen_mclose[0], (100, 100))
             eye_bin_mopen_mclose[1] = cv2.resize(eye_bin_mopen_mclose[1], (100, 100))
             
+            # Łączenie obrazów w jedną tablicę numpy
             numpy_horizontal_concat = np.concatenate((eye_bin[0], eye_bin[1], eye_bin_mopen_mclose[0], eye_bin_mopen_mclose[1]), axis=1)
-            cv2.imshow('Bin eyes for testing', numpy_horizontal_concat)
+            cv2.imshow('Bin eyes for testing', numpy_horizontal_concat) # Wyświetlanie obrazów binarnych
 
         # Wyświetlanie obrazu głównego
         cv2.imshow('Eye Detection', frame)
@@ -159,5 +187,6 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
+# Zakończenie programu
 cap.release()
 cv2.destroyAllWindows()
