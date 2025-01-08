f4a588a3655a763c813cf42e104bc8846d8c4998
szniobo <122813024+FastFoot3@users.noreply.github.com>
2025-01-08 21:06:41 +0100

    Kalibracja światłem
    Nie działa dobrze, ale to jest z pewnością zależne od sprzętu jaki się ma, dodatkowa funkcja zawsze lepiej

--- eye_detect.py
+++ eye_detect.py
@@ -28,8 +28,8 @@ cv2.createTrackbar('Eye thresh', 'Eye Detection', eye_thresh, max_eye_thresh, up
 
 
 # Inicjalizacja wartości progu światła
-light_thresh = 15
-max_light_thresh = 100
+light_thresh = 100
+max_light_thresh = 255
 
 # Tworzenie suwaka do dynamicznego dostosowywania wartości progu światła
 cv2.createTrackbar('Light thresh', 'Eye Detection', light_thresh, max_light_thresh, update_light_threshold)
@@ -124,20 +124,6 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
                     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 255), 2)
 
 
-                # Obliczenie środka oka względem wycinka eye_gray
-                eye_center_x = ew // 2 # obliczenie współrzędnej x
-                eye_center_y = eh // 2 # obliczenie współrzędnej y
-
-                # Rysowanie środka oka
-                if draw_mode == 0:
-                    # Rysowanie lini przecinających środek oka
-                    cv2.line(eye_color, (0, eye_center_y), (ew, eye_center_y), (0, 0, 255), 1)
-                    cv2.line(eye_color, (eye_center_x, 0), (eye_center_x, eh), (0, 0, 255), 1)
-                elif draw_mode == 1:
-                    # Rysowanie małego 'x' w punkcie (eye_center_x, eye_center_y)
-                    cv2.line(eye_color, (eye_center_x - 5, eye_center_y - 5), (eye_center_x + 5, eye_center_y + 5), (0, 0, 255), 1)
-                    cv2.line(eye_color, (eye_center_x + 5, eye_center_y - 5), (eye_center_x - 5, eye_center_y + 5), (0, 0, 255), 1)
-
                 """działanie retval, dst = cv2.threshold(src, thresh, maxval, type)
                 retval (symbol _) - Zwraca wartość użytego progu (można ją pominąć, stąd _)
                 dst (eye_bin) - Obraz binarny wynikowy (czarno-biały obraz źrenicy)
@@ -145,11 +131,6 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
                 thresh (30) - Wartość progu. Piksele są porównywane z tą wartością
                 maxval (255) - Maksymalna wartość, którą piksele przyjmują (tutaj biały)
                 type (cv2.THRESH_BINARY_INV) - Rodzaj progu (tutaj pracujemy na negatywie, czyli piksele ciemniejsze od 30 dają 1)"""
-
-
-                # Progowanie odbicia światła w oku
-                _, light_bin[i] = cv2.threshold(eye_gray, light_thresh, 255, cv2.THRESH_BINARY)
-
 
                 # Progowanie obrazu (binaryzacja)
                 _, eye_bin[i] = cv2.threshold(eye_gray, eye_thresh, 255, cv2.THRESH_BINARY_INV)
@@ -169,19 +150,53 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
                     cv2.CHAIN_APPROX_SIMPLE - Upraszcza kontury, przechowując tylko kluczowe punkty"""
 
                 # Znajdowanie konturów na obrazie binarnym
-                contours, _ = cv2.findContours(eye_bin_mopen_mclose[i], cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                eye_contours, _ = cv2.findContours(eye_bin_mopen_mclose[i], cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+                if light_calibration_mode:
+                    # Progowanie odbicia światła w oku
+                    _, light_bin[i] = cv2.threshold(eye_gray, light_thresh, 255, cv2.THRESH_BINARY)
+
+                    # Znajdowanie konturów na obrazie binarnym odbicia światła
+                    light_countours, _ = cv2.findContours(light_bin[i], cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+                                        
+                    # Obliczenie środka oka względem odbicia światła (jeśli znaleziono kontury)
+                    if light_countours:
+                        # Znalezienie największego konturu odbicia światła
+                        largest_light_contour = max(light_countours, key=cv2.contourArea, default=None)
+
+                        # Obliczenie środka oka względem punktu odbicia światła
+                        (eye_center_x, eye_center_y), _ = cv2.minEnclosingCircle(largest_light_contour)
+                    else:
+                        eye_center_x, eye_center_y = None, None
+                else:
+                    # Obliczenie środka oka względem wycinka eye_gray
+                    eye_center_x = ew // 2 # obliczenie współrzędnej x
+                    eye_center_y = eh // 2 # obliczenie współrzędnej y
+
+
+                # Rysowanie środka oka
+                if eye_center_x is not None and eye_center_y is not None:
+                    if draw_mode == 0:
+                        # Rysowanie lini przecinających środek oka
+                        cv2.line(eye_color, (0, int(eye_center_y)), (ew, int(eye_center_y)), (0, 0, 255), 1)
+                        cv2.line(eye_color, (int(eye_center_x), 0), (int(eye_center_x), eh), (0, 0, 255), 1)
+                    elif draw_mode == 1:
+                        # Rysowanie małego 'x' w punkcie (eye_center_x, eye_center_y)
+                        cv2.line(eye_color, (int(eye_center_x) - 5, int(eye_center_y) - 5), (int(eye_center_x) + 5, int(eye_center_y) + 5), (0, 0, 255), 1)
+                        cv2.line(eye_color, (int(eye_center_x) + 5, int(eye_center_y) - 5), (int(eye_center_x) - 5, int(eye_center_y) + 5), (0, 0, 255), 1)
+
 
                 # Sprawdzenie, czy znaleziono jakieś kontury
-                if contours:
+                if eye_contours:
 
                     # Znaleziono kontury, wybieramy największy kontur
-                    largest_contour = max(contours, key=cv2.contourArea) # Największy kontur prawdopodobnie odpowiada źrenicy
+                    largest_eye_contour = max(eye_contours, key=cv2.contourArea) # Największy kontur prawdopodobnie odpowiada źrenicy
 
                     # Obliczenie środka i promienia okręgu otaczającego kontur
-                    (cx, cy), radius = cv2.minEnclosingCircle(largest_contour) # Znajduje najmniejszy okrąg otaczający kontur i zwraca jego środek i promień (źrenicę)
+                    (cx, cy), radius = cv2.minEnclosingCircle(largest_eye_contour) # Znajduje najmniejszy okrąg otaczający kontur i zwraca jego środek i promień (źrenicę)
                     
                     """# Obliczanie momentów konturu
-                    M = cv2.moments(largest_contour)
+                    M = cv2.moments(largest_eye_contour)
 
                     # Obliczanie środka ciężkości konturu
                     if M["m00"] != 0:
@@ -200,9 +215,12 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
                         cv2.circle(eye_color, (int(cx), int(cy)), 5, (255, 0, 0), -1) # Mały niebieski punkt powinien śledzić środek źrenicy
 
 
-                    # Obliczenie przemieszczenia względem środka oka
-                    displacement_x = cx - eye_center_x
-                    displacement_y = cy - eye_center_y
+                    # Obliczenie przemieszczenia względem środka oka (jeśli istnieje środek oka)
+                    if eye_center_x is not None and eye_center_y is not None:
+                        displacement_x = cx - eye_center_x
+                        displacement_y = cy - eye_center_y
+                    else:
+                        displacement_x, displacement_y = 0, 0
 
                     # Obliczenie czasu działania programu
                     program_runtime = time.time() - start_time
@@ -220,8 +238,20 @@ with open('eye_tracking_data.txt', 'w') as file: # With to bezpieczne zarządzan
             eye_bin_mopen_mclose[1] = cv2.resize(eye_bin_mopen_mclose[1], (100, 100))
             
             # Łączenie obrazów w jedną tablicę numpy
-            numpy_horizontal_concat = np.concatenate((eye_bin[0], eye_bin[1], eye_bin_mopen_mclose[0], eye_bin_mopen_mclose[1]), axis=1)
-            cv2.imshow('Bin eyes for testing', numpy_horizontal_concat) # Wyświetlanie obrazów binarnych
+            numpy_horizontal_concat_eyes = np.concatenate((eye_bin[0], eye_bin[1], eye_bin_mopen_mclose[0], eye_bin_mopen_mclose[1]), axis=1)
+            cv2.imshow('Bin eyes for testing', numpy_horizontal_concat_eyes) # Wyświetlanie obrazów oczu binarnych
+
+
+        # Wyświetlanie odbicia światła binarnego
+        if all(img is not None for img in [light_bin[0], light_bin[1]]):
+            # Skalowanie obrazów do wspólnego rozmiaru
+            light_bin[0] = cv2.resize(light_bin[0], (100, 100))
+            light_bin[1] = cv2.resize(light_bin[1], (100, 100))
+            
+            # Łączenie obrazów w jedną tablicę numpy
+            numpy_horizontal_concat_light = np.concatenate((light_bin[0], light_bin[1]), axis=1)
+            cv2.imshow('Bin reflection for testing', numpy_horizontal_concat_light) # Wyświetlanie obrazów odbicia światła binarnych
+
 
         # Wyświetlanie obrazu głównego
         cv2.imshow('Eye Detection', frame)
